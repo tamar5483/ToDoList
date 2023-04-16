ed10e77f770a352be0ff59f126dc1b0b6bbd8fad
tamar5483 <tamar5483@gmail.com>
2023-04-16 22:22:34 +0300

    init commit

---
+++ User.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ToDpAPI;
+
+public partial class User
+{
+    public int Id { get; set; }
+
+    public string Name { get; set; } = null!;
+
+    public string Password { get; set; } = null!;
+}
